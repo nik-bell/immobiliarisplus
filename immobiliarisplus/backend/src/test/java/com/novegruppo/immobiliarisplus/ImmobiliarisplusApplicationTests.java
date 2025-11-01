9add1762900d43d7f9d6eac8bbb6ab488110e5c2
nik-bell <nicolas.bellotti@edu-its.it>
2025-11-01 17:47:45 +0100

    feat(project): initial setup of Spring Boot backend with Docker and MySQL

---
+++ immobiliarisplus/backend/src/test/java/com/novegruppo/immobiliarisplus/ImmobiliarisplusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.novegruppo.immobiliarisplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImmobiliarisplusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
