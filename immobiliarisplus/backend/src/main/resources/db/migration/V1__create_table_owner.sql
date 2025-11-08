2ab105293f78a1d97c043769b2c6775a6899935f
nik-bell <nicolas.bellotti@edu-its.it>
2025-11-08 12:27:28 +0100

    feat(migrations): add initial database migration files

---
+++ immobiliarisplus/backend/src/main/resources/db/migration/V1__create_table_owner.sql
@@ -0,0 +1,8 @@
+CREATE TABLE owner (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    surname VARCHAR(255) NOT NULL,
+    phone VARCHAR(255),
+    contact_preference ENUM('EMAIL', 'PHONE', 'WHATSAPP') DEFAULT 'EMAIL',
+    intake_date DATETIME DEFAULT CURRENT_TIMESTAMP
+);
