96188d32918a8bbc7691a013677d70da5e0b9fe5
NesmaAbd <nesma.abd@edu-its.it>
2025-11-11 16:33:46 +0100

    feat(owner): create OwnerEntity, ContactPreference enum and OwnerRepository (#39)

---
+++ immobiliarisplus/backend/src/main/java/com/novegruppo/immobiliarisplus/repository/OwnerRepository.java
@@ -0,0 +1,7 @@
+package com.novegruppo.immobiliarisplus.repository;
+
+import com.novegruppo.immobiliarisplus.entities.Owner;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OwnerRepository extends JpaRepository<Owner, Integer> {
+}
