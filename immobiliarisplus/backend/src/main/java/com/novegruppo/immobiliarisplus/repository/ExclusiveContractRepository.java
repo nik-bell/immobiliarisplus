a9a612a1739452295c28c27ad8db9459d5b88594
nik-bell <nicolas.bellotti@edu-its.it>
2025-11-11 19:01:34 +0100

    feat(DAO): add exclusiveContract repository

---
+++ immobiliarisplus/backend/src/main/java/com/novegruppo/immobiliarisplus/repository/ExclusiveContractRepository.java
@@ -0,0 +1,7 @@
+package com.novegruppo.immobiliarisplus.repository;
+
+import com.novegruppo.immobiliarisplus.entities.ExclusiveContract;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExclusiveContractRepository extends JpaRepository<Owner, Integer> {
+}
