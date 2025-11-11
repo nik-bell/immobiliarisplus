598858c09a70876dc29719771dd7a542813610ca
Matteo Paglietta <Matteo.Paglietta@edu-its.it>
2025-11-11 13:46:54 +0100

    fix(cookie policy e privacy policy): fixato i moduli dei cookie policy e privacy policy

--- immobiliarisplus/frontend/src/components/Footer.jsx
+++ immobiliarisplus/frontend/src/components/Footer.jsx
@@ -1,7 +1,13 @@
+import CookiePolicy from './CookiePolicy'
+import PrivacyPolicy from './PrivacyPolicy'
+
+
 function Footer() {
   return (
     <footer>
       <h3>Questo è il footer</h3>
+      <CookiePolicy />
+      <PrivacyPolicy />
     </footer>
   );
 }
