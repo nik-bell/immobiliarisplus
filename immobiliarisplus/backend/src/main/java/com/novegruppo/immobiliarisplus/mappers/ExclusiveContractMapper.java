90c4c028cda9ef51e77b77b0c1c022db18f4ac44
nik-bell <nicolas.bellotti@edu-its.it>
2025-11-17 10:53:15 +0100

    feat(mappers): add contract mapper

---
+++ immobiliarisplus/backend/src/main/java/com/novegruppo/immobiliarisplus/mappers/ExclusiveContractMapper.java
@@ -0,0 +1,19 @@
+package com.novegruppo.immobiliarisplus.mappers;
+
+import com.novegruppo.immobiliarisplus.entities.ExclusiveContract;
+import org.mapstruct.*;
+
+@Mapper(componentModel = "spring")
+public interface ExclusiveContractMapper {
+    @Mapping(source = "property.id", target = "propertyId")
+    ExclusiveContractDTO toDTO(ExclusiveContract entity);
+
+    @Mapping(target = "property", ignore = true)
+    @Mapping(target = "id", ignore = true)
+    ExclusiveContract fromCreate(ExclusiveContractDTO dto);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "property", ignore = true)
+    @Mapping(target = "id", ignore = true)
+    void updateEntityFromUpdate(ExclusiveContractDTO dto, @MappingTarget ExclusiveContract entity);
+}
