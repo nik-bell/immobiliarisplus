9d5be5c3f99417d7423710e26a18c6a37dd000cc
nik-bell <nicolas.bellotti@edu-its.it>
2025-11-17 10:51:27 +0100

    feat(mappers): add employee mapper

---
+++ immobiliarisplus/backend/src/main/java/com/novegruppo/immobiliarisplus/mappers/EmployeeMapper.java
@@ -0,0 +1,22 @@
+package com.novegruppo.immobiliarisplus.mappers;
+
+
+import com.novegruppo.immobiliarisplus.entities.Employee;
+import org.mapstruct.*;
+
+@Mapper(componentModel = "spring")
+public interface EmployeeMapper {
+    @Mapping(source = "user.id", target = "userId")
+    EmployeeDTO toDTO(Employee entity);
+
+    @Mapping(target = "user", ignore = true)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    Employee fromCreate(EmployeeDTO dto);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "user", ignore = true)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    void updateEntityFromUpdate(EmployeeDTO dto, @MappingTarget Employee entity);
+}
