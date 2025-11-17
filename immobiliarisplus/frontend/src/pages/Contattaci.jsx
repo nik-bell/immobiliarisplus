ca118ca3729b932f97b574aef61ee5fa1ba1e129
Pagliez <matteo.paglietta@edu-its.it>
2025-11-17 21:44:18 +0100

    feat(assistenza): created FAQ section in support section

--- immobiliarisplus/frontend/src/pages/Contattaci.jsx
+++ immobiliarisplus/frontend/src/pages/Contattaci.jsx
@@ -1,14 +1,16 @@
 import HeroAssistenza from "../sections/HeroAssistenza";
 import ContattiAssistenza from "../sections/ContattiAssistenza";
 import FormAssistenza from "../sections/FormAssistenza";
+import FAQAssistenza from "../sections/FAQAssistenza";
 
 function Contattaci() {
   return(
     <>
     <HeroAssistenza />
-    <div className="container">
+    <div className="container xl:px-32">
       <ContattiAssistenza />
       <FormAssistenza />
+      <FAQAssistenza />
     </div>
     </>
   )
