9a54c1184cb44676b767767108d1f0f06a0e2485
nik-bell <nicolas.bellotti@edu-its.it>
2025-11-17 10:57:06 +0100

    feat(mappers): add owner mapper

---
+++ immobiliarisplus/backend/src/main/java/com/novegruppo/immobiliarisplus/mappers/OwnerMapper.java
@@ -0,0 +1,21 @@
+package com.novegruppo.immobiliarisplus.mappers;
+
+import com.novegruppo.immobiliarisplus.dtos.OwnerDTO;
+import com.novegruppo.immobiliarisplus.entities.Owner;
+import org.mapstruct.*;
+
+@Mapper(componentModel = "spring")
+public interface OwnerMapper {
+
+    @Mapping(source = "intakeDate", target = "createdAt")
+    OwnerDTO toDTO(Owner entity);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "intakeDate", ignore = true)
+    Owner fromCreate(OwnerDTO dto);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "intakeDate", ignore = true)
+    void updateEntityFromUpdate(OwnerDTO dto, @MappingTarget Owner entity);
+}
