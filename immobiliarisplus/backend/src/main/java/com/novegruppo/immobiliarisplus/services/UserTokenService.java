3d3844955e466d21ab0f79708aeb7230cee5fd29
nik-bell <nicolas.bellotti@edu-its.it>
2025-11-17 11:19:34 +0100

    feat(services): add userToken service

---
+++ immobiliarisplus/backend/src/main/java/com/novegruppo/immobiliarisplus/services/UserTokenService.java
@@ -0,0 +1,7 @@
+package com.novegruppo.immobiliarisplus.services;
+
+import com.novegruppo.immobiliarisplus.dtos.UserTokenDTO;
+
+public interface UserTokenService extends CrudService<UserTokenDTO, UserTokenDTO, UserTokenDTO, Integer> {
+}
+
