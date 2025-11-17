2334c03f007b3a492097e05f50db1e4e5347976d
Pagliez <matteo.paglietta@edu-its.it>
2025-11-17 16:43:03 +0100

    feat(migliora-casa): added the note component to the main components and implemented it in the improve home page

--- immobiliarisplus/frontend/src/pages/MiglioraCasa.jsx
+++ immobiliarisplus/frontend/src/pages/MiglioraCasa.jsx
@@ -1,9 +1,13 @@
 import HeroMiglioraCasa from "../sections/HeroMiglioraCasa";
+import Note from "../components/Note";
 
 function MiglioraCasa() {
-  return(
+  return (
     <>
       <HeroMiglioraCasa />
+      <div className="container">
+        <Note className="border border-indigo-900 border-l-4 bg-gray-100 p-4 rounded-lg shadow-sm my-4"> Questi sono esempi di migliorie che possiamo offrire. Per usufruirne, clicca su "Richiedi consulenza" oppure fai una richiesta di preventivo tramite il form di valutazione.</Note>
+      </div>
     </>
   )
 }
