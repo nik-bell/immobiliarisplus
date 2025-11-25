8f23755ff62617c261f67d707063dbe7a615c8e8
Matteo Paglietta <Matteo.Paglietta@edu-its.it>
2025-11-25 15:51:08 +0100

    feat(contratto-esclusiva): creation of the section explaining what the exclusive contract is

--- immobiliarisplus/frontend/src/pages/ContrattoEsclusiva.jsx
+++ immobiliarisplus/frontend/src/pages/ContrattoEsclusiva.jsx
@@ -1,14 +1,15 @@
 import HeroContrattoEsclusiva from "../sections/HeroContrattoEsclusiva";
+import SpiegazioneContratto from '../sections/SpiegazioneContratto';
 
 function ContattoEsclusiva() {
-  return(
-    <>
-    <HeroContrattoEsclusiva />
-    <div className="container xl:px-32">
-
-    </div>
-    </>
-  )
+    return (
+        <>
+            <HeroContrattoEsclusiva />
+            <div className="container xl:px-32">
+                <SpiegazioneContratto />
+            </div>
+        </>
+    )
 }
 
 export default ContattoEsclusiva;
