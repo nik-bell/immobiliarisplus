056ac424bd6b4c8df25a9a4df59135090d0436a8
Matteo  Cecchi <Matteo.Cecchi@edu-its.it>
2025-11-25 15:44:44 +0100

    feat(homapage): add section nostrivalori

--- immobiliarisplus/frontend/src/pages/Homepage.jsx
+++ immobiliarisplus/frontend/src/pages/Homepage.jsx
@@ -1,6 +1,7 @@
 import HeroHomepage from "../sections/HeroHomepage";
 import PercheSceglierci from "../sections/PercheSceglierci";
 import ComeFunzionaHomepage from "../sections/ComeFunzionaHomepage";
+import NostriValori from "../sections/NostriValori";
 
 const HomePage = () => {
   return (
@@ -9,6 +10,7 @@ const HomePage = () => {
       <div className="container">
         <PercheSceglierci />
       </div>
+      <NostriValori />
         <ComeFunzionaHomepage />
     </>
   );
