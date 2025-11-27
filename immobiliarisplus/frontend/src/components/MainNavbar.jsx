5c4d7061ac4dc03a68cfccd1685c89d17b1ed2d4
Matteo  Cecchi <Matteo.Cecchi@edu-its.it>
2025-11-27 22:12:44 +0100

    fix(main-navbar): updated navbar and added new logo in svg

--- immobiliarisplus/frontend/src/components/MainNavbar.jsx
+++ immobiliarisplus/frontend/src/components/MainNavbar.jsx
@@ -5,34 +5,34 @@ const MainNavbar = () => {
   return (
     <nav className="w-full bg-white shadow relative">
       <div className="max-w-7xl mx-auto px-6 py-4 flex items-center justify-between relative z-10">
-        {/* Left - logo scritto */}
-        <div className="flex items-center">
-          <NavLink to="/" className="flex items-center">
-               <img src={LogoSVG} alt="Logo" className="h-11 w-auto" />
-          </NavLink>
-        </div>
+        {/* Left - Logo */}
+        <NavLink to="/" className="flex items-center">
+          <img src={LogoSVG} alt="Logo" className="h-11 w-auto" />
+        </NavLink>
 
-        {/* Right - CTAs */}
-        <div className="flex items-center gap-3">
-          <NavLink to="/valuta-casa">
-            <button className="bg-yellow-400 text-black px-4 py-2 rounded hover:bg-yellow-500">
-              Valuta casa
-            </button>
-          </NavLink>
-        </div>
+        {/* Right - CTA */}
+        <NavLink to="/valuta-casa">
+          <button className="bg-yellow-400 text-black px-4 py-2 rounded hover:bg-yellow-500">
+            Valuta casa
+          </button>
+        </NavLink>
       </div>
 
-      {/* Center - navigation links */}
+      {/* Center - Navigation Links */}
       <div className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 hidden md:flex space-x-6">
-        <NavLink to="/" className="text-gray-600 hover:text-gray-900">
-          Home
-        </NavLink>
-        <NavLink to="/vendi-casa" className="text-gray-600 hover:text-gray-900">
-          Vendi casa
-        </NavLink>
-        <NavLink to="/migliora-casa" className="text-gray-600 hover:text-gray-900">
-          Migliora casa
-        </NavLink>
+        {[
+          { path: "/", label: "Home" },
+          { path: "/vendi-casa", label: "Vendi casa" },
+          { path: "/migliora-casa", label: "Migliora casa" },
+        ].map((link) => (
+          <NavLink
+            key={link.path}
+            to={link.path}
+            className="text-gray-600 hover:text-gray-900"
+          >
+            {link.label}
+          </NavLink>
+        ))}
       </div>
     </nav>
   );
