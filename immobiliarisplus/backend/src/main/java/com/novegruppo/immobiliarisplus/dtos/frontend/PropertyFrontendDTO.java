fedfa3bb88b10bbfcac92bb93aef351c50352069
nik-bell <nicolas.bellotti@edu-its.it>
2025-12-03 22:22:24 +0100

    chore(repos): clean up code, update comments

--- immobiliarisplus/backend/src/main/java/com/novegruppo/immobiliarisplus/dtos/frontend/PropertyFrontendDTO.java
+++ immobiliarisplus/backend/src/main/java/com/novegruppo/immobiliarisplus/dtos/frontend/PropertyFrontendDTO.java
@@ -1,5 +1,7 @@
 package com.novegruppo.immobiliarisplus.dtos.frontend;
 
+// Aggregated DTO for frontend property representation no more used in backend logic
+
 public record PropertyFrontendDTO(
     PropertyInfoDTO property,
     PropertyDetailsDTO details,
--- immobiliarisplus/backend/src/main/java/com/novegruppo/immobiliarisplus/repositories/UserRepository.java
+++ immobiliarisplus/backend/src/main/java/com/novegruppo/immobiliarisplus/repositories/UserRepository.java
@@ -6,5 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+    // find by email for authentication and other purposes.
     Optional<User> findByEmail(String email);
 }
